4a5de5cf02a4d29f01fe716dff86aa73e5310b5d
YasserAlBarbary <48328586+YasserAlBarbary@users.noreply.github.com>
2020-01-21 17:39:21 +0000

    Public version with no API keys or ListIDs specified
    You can get your API key and create a list ID  from mailchimp

---
+++ app.js
@@ -0,0 +1,63 @@
+//jshint esversion:6
+const express=require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+const apikey=;
+const listID=;
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function(req,res){
+  let firstName = req.body.fName;
+  let lastName = req.body.lName;
+  let email = req.body.email;
+  var data = {
+    members: [
+      {
+      email_address: email,
+      status: "subscribed",
+      merge_fields:{
+        FNAME: firstName,
+        LNAME: lastName,
+      }
+    }
+  ]
+  };
+
+  var jsonData = JSON.stringify(data);
+  var options ={
+    url: "https://us4.api.mailchimp.com/3.0/lists/"+listID,
+    method: "POST",
+    headers:{
+      "Authorization": "yasser1 "+apikey
+    },
+    body: jsonData
+  };
+  request(options,function(error,response,body){
+    if(error){
+      res.sendFile(__dirname + "/failure.html");
+    }
+    else{
+      if( response.statusCode ===200){
+        res.sendFile(__dirname+"/success.html");
+      }
+      else{
+        res.sendFile(__dirname + "/failure.html");
+      }
+      console.log(response.statusCode);
+    }
+  });
+
+});
+app.post("/failure",function(req,res){
+  res.redirect("/");
+});
+app.listen(process.env.PORT || 3000,function(){
+  console.log("running on port: 3000");
+});
